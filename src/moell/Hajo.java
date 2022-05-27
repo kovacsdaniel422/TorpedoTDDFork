647058a15c6f5b4c7a85e1504ba3b0078fea9d13
SZF1 A2 <szf1_a2@oktatas.local>
2022-05-27 13:19:52 +0200

    OraiJframeAmeddigJutottam

--- src/moell/Hajo.java
+++ src/moell/Hajo.java
@@ -1,23 +1,30 @@
 package moell;
 
 public class Hajo {
+
     private int[] pozicio;
 
     public Hajo(int[] pozicio) {
         this.pozicio = pozicio;
     }
-    
-    public String talalat(int poz){
+
+    public String talalat(int poz) {
         //eldöntés tétele
         int i = 0;
-        while(i < 3 && !(pozicio[i] == poz)){
+        while (i < 3 && !(pozicio[i] == poz)) {
             i++;
         }
-        
+
         return i < 3 ? "talált" : "mellé";
+        
+        boolean talalat=talalatSzam=pozicio;
     }
-    
-    public int[] getPozicio(){
+
+    public int[] getPozicio() {
         return pozicio;
+    }
+
+    public boolean isElsullyedt() {
+        return isElsullyedt;
     }
 }
--- src/teszt/TorpedoTeszt.java
+++ src/teszt/TorpedoTeszt.java
@@ -2,17 +2,73 @@ package teszt;
 
 import moell.Hajo;
 
+
+
 public class TorpedoTeszt {
-    public static void main(String[] args) {
-        System.out.println("Teszt");
-        
-        new TorpedoTeszt().tesztLovesTalalt(4);
+
+    private Hajo hajo;
+
+    public TorpedoTeszt() {
+        this.hajo = new Hajo(new int[]{2, 3, 4});
+        System.out.println("Tesztesetek");
+        tesztLovesTalalt(4);
+        tesztLovesNemTalalt(5);
+        tesztLovesSullyedt();
+        tesztLovesNemSullyedt();
+        System.out.println("Tesztesetek VÉGE");
     }
-    
-    public String tesztLovesTalalt(int poz){
-        Hajo hajo = new Hajo(new int[]{2,3,4});
+
+    public static void main(String[] args) {
+// new TorpedoTeszt().tesztLovesTalalt(4);
+// new TorpedoTeszt().tesztLovesNemTalalt(5);
+        new TorpedoTeszt();
+    }
+
+    public void tesztLovesTalalt(int poz) {
+        System.out.println("---Találat");
+        hajo = new Hajo(new int[]{2, 3, 4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
-        return "";
+        System.out.println("Találat teszt sikeres");
+    }
+
+    public void tesztLovesNemTalalt(int poz) {
+        System.out.println("---Mellé");
+        hajo = new Hajo(new int[]{2, 3, 4});
+        String t = hajo.talalat(poz);
+        assert t.equals("mellé") : "nem jó a mellé ellenőrzése";
+        System.out.println("Mellé teszt sikeres");
+    }
+
+    public void tesztLovesSullyedt() {
+        System.out.println("---Süllyedt");
+        hajo = new Hajo(new int[]{2, 3, 4});
+        hajo.talalat(2);
+        hajo.talalat(3);
+        hajo.talalat(4);
+        boolean s = hajo.isElsullyedt();
+        assert s == true : "nem jó a süllyedt ellenőrzése";
+        System.out.println("Süllyedt teszt sikeres");
+    }
+
+    public void tesztLovesNemSullyedt() {
+        System.out.println("---Nem süllyedt");
+        hajo = new Hajo(new int[]{2, 3, 4});
+        hajo.talalat(2);
+        hajo.talalat(3);
+        hajo.talalat(1);
+        boolean s = hajo.isElsullyedt();
+        assert s == false : "nem jó a süllyedt ellenőrzése";
+        System.out.println("Nem süllyedt teszt sikeres");
+    }
+
+    /*további nem implementált esetek: */
+    public void tesztHajoJoHelyenVan() {
+    }
+
+    public void tesztTalalatokSzama() {
+    }
+
+    public void tesztHolSerult() {
     }
 }
